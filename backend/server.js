cbf37c3a62238630e88581bdf739dcad80432976
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-11-30 17:08:00 +0630

    build userRouters component

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRouters = require("./routes/userRouters");
 
 dotenv.config();
 connectDB();
@@ -12,15 +13,17 @@ app.get("/", (req, res) => {
 	res.send("API is Running");
 });
 
-app.get("/api/chat", (req, res) => {
-	res.send(chats);
-});
+app.use("/api/user", userRouters);
 
-app.get("/api/chat/:id", (req, res) => {
-	// console.log(req.params.id);
-	const singleChat = chats.find((c) => c._id === req.params.id);
-	res.send(singleChat);
-});
+// app.get("/api/chat", (req, res) => {
+// 	res.send(chats);
+// });
+
+// app.get("/api/chat/:id", (req, res) => {
+// 	// console.log(req.params.id);
+// 	const singleChat = chats.find((c) => c._id === req.params.id);
+// 	res.send(singleChat);
+// });
 
 const PORT = process.env.PORT || 5000;
 
