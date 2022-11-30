8bd234529f90789bceb467766fb1629e00675ade
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-11-30 10:44:52 +0630

    create mondb component

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
 app.get("/", (req, res) => {
 	res.send("API is Running");
@@ -21,4 +24,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`.yellow.bold));
