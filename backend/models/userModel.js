d4e5e052130f2ae1cfbcc313363a897f68fed736
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-12-01 10:07:23 +0630

    build the userControllers component

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema(
 	{
 		name: { type: String, require: true },
-		email: { type: String, require: true },
+		email: { type: String, require: true, unique: true },
 		password: { type: String, require: true },
 		pic: {
 			type: String,
-			require: true,
 			default:
 				"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
 		},
