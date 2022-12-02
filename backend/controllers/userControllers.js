058a35c66157744334e5bb868a1aeafd3056ad6e
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-12-02 10:22:39 +0630

    backend.. user information

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -27,10 +27,10 @@ const registerUser = asyncHandler(async (req, res) => {
 	if (user) {
 		res.status(201).json({
 			_id: user._id,
-			name: user._name,
-			email: user._email,
-			pic: user._pic,
-			token: generateToken(user_id),
+			name: user.name,
+			email: user.email,
+			pic: user.pic,
+			token: generateToken(user._id),
 		});
 	} else {
 		res.status(400);
@@ -38,4 +38,23 @@ const registerUser = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { registerUser };
+const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			pic: user.pic,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(401);
+		throw new Error("Invalid Email or Password");
+	}
+});
+
+module.exports = { registerUser, authUser };
