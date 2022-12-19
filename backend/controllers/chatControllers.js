f5d61df47083623a2c572b21da6e1cb85738bcf2
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-12-19 09:56:45 +0630

    group chat create and rename

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -70,4 +70,56 @@ const fetchChats = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { accessChat, fetchChats };
+const createGroupChat = asyncHandler(async (req, res) => {
+	if (!req.body.users || !req.body.name) {
+		return res.status(400).send({ message: "Please Fill all the feilds" });
+	}
+	var users = JSON.parse(req.body.users);
+	if (users.length < 2) {
+		return res
+			.status(400)
+			.send("More than 2 users are required to form a group chat");
+	}
+	users.push(req.user);
+
+	try {
+		const groupChat = await Chat.create({
+			chatName: req.body.name,
+			users: users,
+			isGroupChat: true,
+			groupAdmin: req.user,
+		});
+		const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+			.populate("users", "-password")
+			.populate("groupAdmin", "-password");
+
+		res.status(200).json(fullGroupChat);
+	} catch (error) {
+		res.status(400);
+		throw new Error(error.message);
+	}
+});
+
+const renameGroup = asyncHandler(async (req, res) => {
+	const { chatId, chatName } = req.body;
+	const updatedChat = await Chat.findByIdAndUpdate(
+		chatId,
+		{
+			chatName,
+		},
+		{
+			new: true,
+		}
+	)
+		.populate("users", "-password")
+		.populate("groupAdmin", "-password");
+
+	if (!updatedChat) {
+		res.status(400);
+		throw new Error("Chat Not Found");
+	} else {
+		res.json(updatedChat);
+	}
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup };
