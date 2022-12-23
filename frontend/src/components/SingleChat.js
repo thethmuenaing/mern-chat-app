5cc5339f29de2ec47e5a1400348bb2593d22b580
thethmuenaing <thethmuenaing22112002@gmail.com>
2022-12-23 22:19:51 +0630

    Fetch messages and send message

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,13 +1,100 @@
 import { ArrowBackIcon } from "@chakra-ui/icons";
-import { Box, IconButton, Text } from "@chakra-ui/react";
-import React from "react";
+import {
+	Box,
+	FormControl,
+	IconButton,
+	Input,
+	Spinner,
+	Text,
+	useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
 import ProfileModal from "./miscellaneous/ProfileModal";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+	const [messages, setMessages] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [newMessage, setNewMessage] = useState();
 	const { user, selectedChat, setSelectedChat } = ChatState();
+
+	const toast = useToast();
+
+	const fetchMessages = async () => {
+		if (!selectedChat) return;
+
+		try {
+			const config = {
+				headers: {
+					Authorization: `Bearer ${user.token}`,
+				},
+			};
+			setLoading(true);
+			const { data } = await axios.get(
+				`/api/message/${selectedChat._id}`,
+				config
+			);
+			console.log("messages ", messages);
+			setMessages(data);
+			setLoading(false);
+		} catch (error) {
+			toast({
+				title: "Error Occured!",
+				description: "Failed to Load the Messages",
+				status: "error",
+				duration: 5000,
+				isClosable: true,
+				position: "bottom",
+			});
+		}
+	};
+
+	useEffect(() => {
+		fetchMessages();
+	}, [selectedChat]);
+
+	const sendMessage = async (event) => {
+		if (event.key === "Enter" && newMessage) {
+			try {
+				const config = {
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${user.token}`,
+					},
+				};
+				setNewMessage("");
+				const { data } = await axios.post(
+					`/api/message`,
+					{
+						content: newMessage,
+						chatId: selectedChat._id,
+					},
+					config
+				);
+
+				console.log("data ", data);
+				setMessages([...messages, data]);
+			} catch (error) {
+				toast({
+					title: "Error Occured!",
+					description: "Failed to send the Message",
+					status: "error",
+					duration: 5000,
+					isClosable: true,
+					position: "bottom",
+				});
+			}
+		}
+	};
+
+	const typingHandler = (e) => {
+		setNewMessage(e.target.value);
+
+		// Typing Indicator Logic
+	};
 	return (
 		<>
 			{selectedChat ? (
@@ -54,7 +141,27 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 						borderRadius="lg"
 						overflowY="hidden"
 					>
-						{/* Messages Here */}
+						{loading ? (
+							<Spinner
+								size="xl"
+								w={20}
+								h={20}
+								alignSelf="center"
+								margin="auto"
+							/>
+						) : (
+							<div>{/* Messages Here */}</div>
+						)}
+
+						<FormControl onKeyDown={sendMessage} isRequired mt={3}>
+							<Input
+								variant="filled"
+								bg="#E0E0E0"
+								placeholder="Enter a message..."
+								onChange={typingHandler}
+								value={newMessage}
+							/>
+						</FormControl>
 					</Box>
 				</>
 			) : (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,13 @@ import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<ChatProvider>
-		<React.StrictMode>
-			<BrowserRouter>
+	<React.StrictMode>
+		<BrowserRouter>
+			<ChatProvider>
 				<ChakraProvider>
 					<App />
 				</ChakraProvider>
-			</BrowserRouter>
-		</React.StrictMode>
-	</ChatProvider>
+			</ChatProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
